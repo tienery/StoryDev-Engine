244bc11ed4474a09e9308da84ee6e341d0bae036
brutalexcess <selman.luke1@gmail.com>
2014-09-20 17:31:07 +0100

    Added Game Events
    Game Events used in Editor like separate functions for reuse across
    game.

--- Source/Reg.hx
+++ Source/Reg.hx
@@ -4,6 +4,7 @@ import game.Passage;
 import haxe.Json;
 import openfl.Assets;
 import openfl.errors.Error;
+import game.GameEvent;
 
 class Reg
 {
@@ -11,11 +12,15 @@ class Reg
 	public static var title:String;
 
 	private static var _passages:Array<Passage> = [];
+	public static var _gameEvents:Array<GameEvent> = [];
+	public static var _queuedEvents:Array<GameEvent> = [];
 
 	public static function init():Void
 	{
 		var passageString:String = Assets.getText("a/info/passages.json");
 		var passageJSON:Dynamic = Json.parse(passageString);
+		var eventsString:String = Assets.getText("a/info/events.json");
+		var eventsJSON:Dynamic = Json.parse(eventsString);
 
 		title = passageJSON.title.split(" ").join("");
 
@@ -28,14 +33,35 @@ class Reg
 
 			_passages.push(p);
 		}
+		for (i in 0...eventsJSON.events.length) {
+			var e:GameEvent = new GameEvent();
+			e.id = eventsJSON.events[i].id;
+			e.title = eventsJSON.events[i].title;
+			e.triggerState = eventsJSON.events[i].triggerState;
+			
+			_gameEvents.push(e);
+		}
 	}
 
 	public static function getPassage(id:Int):Passage
 	{
 		for (i in 0..._passages.length) if (_passages[i].id == id) return _passages[i];
 		
-		throw new Error("Passage " + id + " doesn't exist");
+		throw new Error("Passage " + id + " does not exist");
 
 		return new Passage();
 	}
+	
+	public static function startEvent(id:Int):Void {
+		for (i in 0..._gameEvents.length) if (_gameEvents[i].id == id) _queuedEvents.push(_gameEvents[i]);
+		
+		throw new Error("Game Event " + id + " does not exist");
+	}
+	
+	public static function stopEvent(id:Int):Void {
+		for (i in 0..._queuedEvents.length) if (_queuedEvents[i].id == id) _queuedEvents.slice(_queuedEvents[i], 1);
+		
+		throw new Error("Game Event " + id + "has already been stopped.");
+	}
+	
 }
---
+++ Source/game/GameEvent.hx
@@ -0,0 +1,19 @@
+package game;
+
+/**
+ * ...
+ * @author Colour Multimedia Enterprises
+ */
+class GameEvent
+{
+
+	public var id:Int;
+	public var title:String;
+	public var triggerState:String;
+	
+	public function new() 
+	{
+		
+	}
+	
+}
